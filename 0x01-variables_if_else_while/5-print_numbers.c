962bb893268f9f60d2dc7dc56cc05dd7e6afff05
SammySlimJ <samanthasalimu@gmail.com>
2024-06-21 16:06:38 -0700

    Implement Task 5, Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int single_digit;
+	for (single_digit = 0; single_digit <= 9; single_digit++)
+	{
+		printf("%d", single_digit);
+	}
+	printf("\n");
+	return (0);
+}
