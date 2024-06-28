05d768f6d82ba501f0469ff2d4c968966dc486dc
SammySlimJ <samanthasalimu@gmail.com>
2024-06-28 11:42:15 -0700

    Implement Task 2, Multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: the integer to be multiplied
+ *@b: the integer to be multiplied
+ *
+ *Description: intmul multiplies two integers
+ *Return: a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
