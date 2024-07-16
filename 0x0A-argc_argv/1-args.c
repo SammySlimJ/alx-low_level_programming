251dafae3d0a345939aa40c90284eb342947db12
SammySlimJ <samanthasalimu@gmail.com>
2024-07-16 06:19:17 -0700

    Implement Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *@argc: the argument that counts the number of command lines
+ *@argv: array of strings where each element points to a command line argument
+ *
+ *Description: this argument prinst the number of arguments passed into it
+ *Return: Always zero
+ */
+int main ( int argc, char **argv __attribute__((unused)))
+{
+	printf ("%d\n", argc - 1);
+	return (0);
+}
