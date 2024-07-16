a5dcab300c89279be6015822b9b0a6a70e7e098b
SammySlimJ <samanthasalimu@gmail.com>
2024-07-16 05:45:33 -0700

    add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include <stdio.h> // Include the standard input/output library
-
+#include "main.h"
+#include <stdio.h>
 /**
  * main - a program that prints its name, followed by a new line
  * @argc: the argument that counts the number of command lines
@@ -7,7 +7,7 @@
  * Description: This program prints its name followed by a new line
  * Return: Always zero
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
   printf("%s\n", argv[0]);
   return (0);
